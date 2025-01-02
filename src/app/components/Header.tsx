a9532bea425897b13b0b1d867fa9a2ec685396ce
Monehin <e.monehin@live.com>
2025-01-02 06:43:07 +0100

    Update Navbar and Favicon

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,7 +1,6 @@
+import Navbar from "@/components/Navbar";
 import { ModalProvider } from "@/components/ui/animated-modal";
 import { checkUser } from "@/lib/checkUser";
-import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { ThemeToggle } from "./ThemeToggle";
 import { ProfileUpdateModal } from "./modals/ProfileUpdateModal";
 const Header = async () => {
   const user = await checkUser();
@@ -18,34 +17,7 @@ const Header = async () => {
     );
   }
 
-  return (
-    <nav className="flex justify-between items-center mx-8 h-16">
-      <div className="navbar-container">
-        <h2 className="text-lg font-extrabold">AFSS 2007</h2>
-      </div>
-      <ul className="flex gap-x-8">
-        {user && user?.verified && (
-          <>
-            <li>Home</li>
-            <li>Giggs</li>
-            <li>Events</li>
-            <li>Forum</li>
-          </>
-        )}
-      </ul>
-      <div className="flex items-center gap-x-8">
-        <div>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <SignedOut>
-            <SignInButton />
-          </SignedOut>
-        </div>
-        <ThemeToggle />
-      </div>
-    </nav>
-  );
+  return <Navbar />;
 };
 
 export default Header;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,159 @@
+"use client";
+import { ThemeToggle } from "@/app/components/ThemeToggle";
+import { cn } from "@/lib/utils";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Menu } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+import { Button, buttonVariants } from "./ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+
+const Navbar = () => {
+  return (
+    <>
+      <DesktopNavbar />
+      <MobileNavbar />
+    </>
+  );
+};
+
+const items = [
+  { label: "Home", link: "/" },
+  { label: "Giggs", link: "/giggs" },
+  { label: "Events", link: "/events" },
+  { label: "Forum", link: "/forum" },
+  { label: "Donations", link: "/Donations" },
+];
+
+function DesktopNavbar() {
+  return (
+    <div className="hidden md:flex border-separate border-b bg-background">
+      <nav className="flex w-full items-center justify-between px-8">
+        <h2 className="text-lg font-extrabold">AFSS07</h2>
+
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+          <div className="flex h-full">
+            {items.map((item) => (
+              <NavBarItems
+                key={item.label}
+                label={item.label}
+                link={item.link}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex items-center gap-4">
+          <ThemeToggle />
+          <div>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+function NavBarItems({
+  label,
+  link,
+  onClick,
+}: {
+  label: string;
+  link: string;
+  onClick?: () => void;
+}) {
+  const pathname = usePathname();
+  const isActive = pathname === link;
+
+  return (
+    <div className="relative flex items-center">
+      <Link
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "w-full justify-start sm:text-sm md:text-base text-muted-foreground hover:text-foreground",
+          isActive && "text-foreground"
+        )}
+        onClick={() => onClick && onClick()}
+        href={link}
+      >
+        {label}
+      </Link>
+      {isActive && (
+        <div className="absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-x-1/2 rounded-xl  bg-foreground md:block"></div>
+      )}
+    </div>
+  );
+}
+
+function MobileNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="block border-separate bg-background md:hidden">
+      <nav className="container flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size={"icon"}>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="flex flex-col sm:w-[540px]" side={"left"}>
+            <SheetHeader>
+              <SheetTitle>AFSS07</SheetTitle>
+              <SheetDescription>Preseverance Breeds Success</SheetDescription>
+            </SheetHeader>
+            <div className="flex flex-col gap-1">
+              {items.map((item) => (
+                <NavBarItems
+                  key={item.label}
+                  label={item.label}
+                  link={item.link}
+                  onClick={() => setIsOpen(false)}
+                />
+              ))}
+            </div>
+            <div className="flex items-center gap-2">
+              <div>
+                <SignedIn>
+                  <UserButton />
+                </SignedIn>
+                <SignedOut>
+                  <SignInButton />
+                </SignedOut>
+              </div>
+            </div>
+          </SheetContent>
+        </Sheet>
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+          <h2 className="text-lg font-extrabold">AFSS07</h2>
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeToggle />
+          <div>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
