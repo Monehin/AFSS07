8f0cee64467d95e45509dfa326c85dbb47c4e5f9
Monehin <e.monehin@live.com>
2025-01-11 16:54:54 +0100

    update profile schema

--- src/app/onboarding/_actions/createProfile.ts
+++ src/app/onboarding/_actions/createProfile.ts
@@ -3,6 +3,7 @@
 import { auth } from "@clerk/nextjs/server";
 import { Prisma, PrismaClient, Profile } from "@prisma/client";
 import { revalidatePath } from "next/cache";
+import { currentUser } from "@clerk/nextjs/server";
 
 const prisma = new PrismaClient();
 
@@ -15,15 +16,15 @@ export async function createProfile(
   data: Prisma.ProfileCreateInput
 ): Promise<ProfileResponse> {
   try {
-    const { userId } = await auth();
+    const user = await currentUser();
 
-    if (!userId) {
+    if (!user) {
       throw new Error("You must be logged in to create your profile.");
     }
 
     // Check if a profile already exists for the user
     const existingProfile = await prisma.profile.findUnique({
-      where: { userId },
+      where: { userId: user.id },
     });
 
     if (existingProfile) {
@@ -31,7 +32,14 @@ export async function createProfile(
     }
 
     // Create the Profile
-    const profile = await prisma.profile.create({ data });
+    const profile = await prisma.profile.create({
+      data: {
+        ...data,
+        firstName: user.firstName || user.fullName,
+        lastName: user.lastName,
+        imageUrl: user.imageUrl,
+      },
+    });
 
     // Revalidate the path to ensure the changes are reflected on the front end
     revalidatePath("/");
