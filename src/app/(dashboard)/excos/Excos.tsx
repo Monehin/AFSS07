c52701728c2a3f1aaeed9ee1476ead793dd8e283
Monehin <e.monehin@live.com>
2025-02-05 10:28:14 +0100

    update style

--- src/app/(dashboard)/excos/Excos.tsx
+++ src/app/(dashboard)/excos/Excos.tsx
@@ -7,25 +7,25 @@ const excos = [
   {
     role: "Coordinator",
     name: "Ini Usen",
-    icon: <User size={40} className="text-blue-500" />,
+    icon: <User size={30} className="text-blue-500" />,
     gif: "https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExaDNxOXdwY2s0ZzYwb211bWk3cHEyODNpeWJvMmhqaDYwYTdiZWZhMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/f4wKcybxIKZIJEWCut/giphy.gif",
   },
   {
     role: "Admin Secretary",
     name: "Bolu Salami",
-    icon: <Clipboard size={40} className="text-pink-500" />,
+    icon: <Clipboard size={30} className="text-pink-500" />,
     gif: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExanJ0bm4zZTAxcHppM3JrNDdlNHN1NTBvbGZ5Y3dpMHRwZDJxZ3I4biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/vhizKXA3BvjeozRk8A/giphy.gif",
   },
   {
     role: "Treasurer",
     name: "Amaka Osueze",
-    icon: <DollarSign size={40} className="text-green-500" />,
+    icon: <DollarSign size={30} className="text-green-500" />,
     gif: "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExdHF4bGIyajV5c21hZ3N1MjIxZ3dtbGJ1aDZyczF5bGhrbzQzZWd0YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2JeQO72XbrQk4wNdbp/giphy.gif",
   },
   {
     role: "Public Relations Officer",
     name: "Adedotun Somoye",
-    icon: <Megaphone size={40} className="text-yellow-500" />,
+    icon: <Megaphone size={30} className="text-yellow-500" />,
     gif: "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcG9tbHllYmR0bTZ5b3FrbG5hcWIwYTYyZzQ0bzVkeHYwN3c5Y252MiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2Y8Iq3xe121Ba3hUAM/giphy.gif",
   },
 ];
@@ -33,19 +33,18 @@ const excos = [
 const ExcosDisplay = () => {
   return (
     <div className="min-h-screen bg-gradient-to-rpy-12 px-4">
-      <h2 className="text-2xl font-extrabold text-center m-8">
+      <h2 className="text-2xl font-extrabold text-center m-6">
         AFSS07 2025 Excos
       </h2>
-      <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-4">
         {excos.map((exco, index) => (
           <div
             key={index}
             className="exco-card rounded-xl shadow p-6 flex flex-col items-center transform transition-transform duration-300 hover:scale-105 hover:shadow-2xl"
-            // Stagger each card's animation by increasing delay
             style={{ animationDelay: `${index * 0.2}s` }}
           >
-            <div className="mb-4">{exco.icon}</div>
-            <div className="mb-4">
+            <div className="mb-2">{exco.icon}</div>
+            <div className="mb-2">
               <Image
                 height={200}
                 width={200}
