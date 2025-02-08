98ba36f72576dbff674fdf14fb0e6133c2d7222a
Monehin <e.monehin@live.com>
2025-02-08 15:28:41 +0100

    align social media items

--- src/app/(dashboard)/home/components/SocialMediaList.tsx
+++ src/app/(dashboard)/home/components/SocialMediaList.tsx
@@ -9,7 +9,7 @@ const SocialMediaList = ({
   socialMediaLinks: SocialMediaLink[];
 }) => {
   return (
-    <div className="flex flex-wrap gap-x-2 gap-y-3">
+    <div className="flex justify-start items-center flex-wrap gap-x-2 gap-y-3">
       {socialMediaLinks && Array.isArray(socialMediaLinks)
         ? socialMediaLinks.map(({ url, platform }: SocialMediaLink) => {
             const media = platformOptions.find(
