f7ef51b471b506c07951494303f1601fd49c4237
Monehin <e.monehin@live.com>
2025-02-09 14:25:15 +0100

    sort social media

--- src/app/(dashboard)/home/components/SocialMediaList.tsx
+++ src/app/(dashboard)/home/components/SocialMediaList.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import LogoIcon from "@/components/LogoIcon";
 import { platformOptions } from "@/utils/platformOptions";
 import { SocialMediaLink } from "@prisma/client";
@@ -8,27 +9,30 @@ const SocialMediaList = ({
 }: {
   socialMediaLinks: SocialMediaLink[];
 }) => {
+  const sortedSocialMediaLinks =
+    socialMediaLinks && Array.isArray(socialMediaLinks)
+      ? [...socialMediaLinks].sort((a, b) =>
+          a.platform.localeCompare(b.platform)
+        )
+      : [];
+
   return (
     <div className="flex justify-start items-center flex-wrap gap-x-2 gap-y-3">
-      {socialMediaLinks && Array.isArray(socialMediaLinks)
-        ? socialMediaLinks.map(({ url, platform }: SocialMediaLink) => {
-            const media = platformOptions.find(
-              (option) => option.id === platform
-            );
-            const color = media?.color;
-            return (
-              <a
-                key={platform}
-                href={`${media?.basePath}${url}`}
-                target="_blank"
-                rel="noreferrer"
-                style={{ color }}
-              >
-                <LogoIcon icon={platform} />
-              </a>
-            );
-          })
-        : null}
+      {sortedSocialMediaLinks.map(({ url, platform }: SocialMediaLink) => {
+        const media = platformOptions.find((option) => option.id === platform);
+        const color = media?.color;
+        return (
+          <a
+            key={platform}
+            href={`${media?.basePath}${url}`}
+            target="_blank"
+            rel="noreferrer"
+            style={{ color }}
+          >
+            <LogoIcon icon={platform} />
+          </a>
+        );
+      })}
     </div>
   );
 };
